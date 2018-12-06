9ecd1c1b3a90529e68ad01a1e5ba0472fe0faaee
18581489108 <18581489108@163.com>
2018-12-06 11:55:23 +0800

    protobuf测试

--- frontend/kanban/src/index.tsx
+++ frontend/kanban/src/index.tsx
@@ -1,14 +1,15 @@
+/*
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-
+*/
 /*
 let loader = new KanbanLoader(live2dData);
 loader.load();
 */
 
-import { WebSocketClient } from "./modules/testProtobuf";
+//import { WebSocketClient } from "./modules/testProtobuf";
 
-let webSocket = new WebSocketClient("ws://localhost:8090/ws");
+//let webSocket = new WebSocketClient("ws://localhost:8090/ws");
 
 /*
 import {example} from './message/example';
@@ -16,3 +17,21 @@ let loginRequest = example.LoginRequest.create({uuid: 9});
 let arr = example.LoginRequest.encode(loginRequest).finish();
 console.info(arr);
 */
+
+
+
+import { example } from './message/example';
+
+let loginRequest = example.LoginRequest.create({uuid: 9});
+
+show(loginRequest);
+function show(arg : any) {
+    //arg.test();
+    //console.log(arg.constructor.name);
+    /*
+    let arr = arg.constructor.encode(arg).finish() as Uint8Array;
+    console.log(arr);
+    */
+    //console.log(arg.constructor);
+}
+                                                                                                                                
